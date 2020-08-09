49d562c2e35899b4358bd5bf35acf406e625d703
sudoajay <sudoajay@gmail.com>
2020-08-09 07:59:40 +0530

    Added Firebase Notification

---
+++ app/src/main/java/com/sudoajay/pdfviewer/firebase/MyFirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package com.sudoajay.pdfviewer.firebase
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyFirebaseMessagingService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+}
